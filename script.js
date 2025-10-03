d818be97301d921ae00da4ab5e4473515532ea38
Mike Laptop <hassonmike@gmail.com>
2025-10-03 06:07:45 -0400

    progress

---
+++ script.js
@@ -0,0 +1,8 @@
+// check and uncheck function logic:
+//  Crossed out and greyes out li. 
+//  Moves it to the bottom of the list.
+
+// Function to capture New Goal input and add it to the list
+
+// test
+
