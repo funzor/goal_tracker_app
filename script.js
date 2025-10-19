ca8018823240f931dcfe62bf0e11e4f72c8c6691
Mike Desktop <mh.codeprojects@gmail.com>
2025-10-19 15:14:43 -0400

    added urgency

--- script.js
+++ script.js
@@ -3,21 +3,38 @@ let addGoalButton = document.getElementById("add-goal-button")
 let deleteAllGoalsButton = document.getElementById("delete-all-goals-button");
 let goalsList = document.getElementById('goals-list');
 
+let selectedImportance;
+let selectedUrgency;
+
 addGoalButton.addEventListener('click', getNewGoalText);
 
 function getNewGoalText() {
     newGoalText = document.getElementById("new-goal-input").value;
+    selectedImportance = document.querySelector('input[name="importance"]:checked');
+    selectedUrgency = document.querySelector('input[name="urgency"]:checked');
     let createdListItem = document.createElement('li');
     let newItemCheckbox = document.createElement('input');
     newItemCheckbox.type = 'checkbox';
     newItemCheckbox.addEventListener('change', toggleGoalComplete)
+    let goalTextSpan = document.createElement('span');
+    goalTextSpan.classList.add('goal-text');
+    goalTextSpan.textContent = newGoalText;
     let parentElementOfNewGoalText = document.getElementById('goals-list');   
     parentElementOfNewGoalText.appendChild(createdListItem);
     let parentElementOfNewListItem = createdListItem; 
     parentElementOfNewListItem.appendChild(newItemCheckbox);
-    let newGoalTextNode = document.createTextNode(newGoalText);
-    createdListItem.appendChild(newGoalTextNode);
+    createdListItem.appendChild(goalTextSpan);
+    // let newGoalTextNode = document.createTextNode(newGoalText);
+    // createdListItem.appendChild(newGoalTextNode);
+    let createdMatrixBadge = document.createElement('span')
+    createdMatrixBadge.classList.add('matrix-badge')
+    createdListItem.appendChild(createdMatrixBadge);
+    createdMatrixBadge.textContent = (`Urgency: ${selectedUrgency.value}. Importance: ${selectedImportance.value}`)
+    createdListItem.dataset.urgency = selectedUrgency.value;
+    createdListItem.dataset.importance = selectedImportance.value;
+
     document.getElementById('new-goal-input').value = "";
+    // <-- clear radio bubbles    
     updateGoalCount();
 }
 
@@ -49,11 +66,17 @@ function saveGoals() {
     goalsListArray = [] //start fresh, avoid concetatenation 
     for (const goalLine of goalsList.children) {
         const goalStatus = goalLine.querySelector('input').checked;
-        const goalText = goalLine.innerText;
+        const goalText = goalLine.querySelector('.goal-text').textContent;
+        const goalUrgency =goalLine.dataset.urgency;
+        const goalImportance= goalLine.dataset.importance;
         const goalDataForJSON = {
             text: goalText,
-            completed: goalStatus
+            completed: goalStatus,
+            importance: goalImportance,
+            urgency: goalUrgency
         }
+
+
         goalsListArray.push(goalDataForJSON);
         
     } ;
@@ -72,9 +95,21 @@ function buildListFromLocalStorage(goalsArray) {
         if (goalDataFromJSON.completed === true) {
             loadedListItem.classList.add('completed'); 
         } 
-        let loadedGoalTextNode = document.createTextNode(goalDataFromJSON.text)
+       
+        let loadedListTextSpan = document.createElement('span');
+        loadedListTextSpan.classList.add('goal-text');
+        loadedListTextSpan.textContent = goalDataFromJSON.text;
+   
+
         loadedListItem.appendChild(loadedItemCheckbox);
-        loadedListItem.appendChild(loadedGoalTextNode);
+        loadedListItem.appendChild(loadedListTextSpan);
+        loadedListItem.dataset.importance = goalDataFromJSON.importance;
+        loadedListItem.dataset.urgency = goalDataFromJSON.urgency;
+        let loadedMatrixBadge = document.createElement('span');
+        loadedMatrixBadge.textContent = `Urgency: ${goalDataFromJSON.urgency}. Importance: ${goalDataFromJSON.importance}`;
+        loadedListItem.appendChild(loadedMatrixBadge);
+
+        loadedMatrixBadge.classList.add('matrix-badge')
         goalsList.appendChild(loadedListItem);
     
     }
